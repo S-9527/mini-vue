37d29fd5c6fbc7c885a723b6ff11519cf1a2f4ff
kid <2713786149@qq.com>
2024-03-26 04:18:34 +0800

    实现 proxyRefs 功能

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -50,3 +50,18 @@ export function isRef<T>(ref: RefImpl<T> | T): ref is RefImpl<T> {
 export function unRef<T>(ref: RefImpl<T> | T) {
     return isRef(ref) ? ref.value : ref
 }
+
+export function proxyRefs(objectWithRefs: any) {
+    return new Proxy(objectWithRefs, {
+        get(target, key) {
+            return unRef(Reflect.get(target, key))
+        },
+        set(target, key, value) {
+            if (isRef(target[key]) && !isRef(value)) {
+                return target[key].value = value
+            } else {
+                return Reflect.set(target, key, value)
+            }
+        }
+    })
+}
