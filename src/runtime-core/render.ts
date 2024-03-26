4152ec22dc6a58e3636958dcd4cc5393a45b3fa5
kid <2713786149@qq.com>
2024-03-26 23:54:57 +0800

    实现初始化 element 主流程

--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,11 +1,44 @@
 import { createComponentInstance, setupComponent } from "./component";
+import { isObject } from "../shared";
 
 export function render(vnode: any, container: any) {
     patch( vnode, container)
 }
 
 function patch(vnode: any, container: any) {
-    processComponent(vnode, container)
+    if (typeof vnode.type === 'string') {
+        processElement(vnode, container)
+    } else if (isObject(vnode.type)) {
+        processComponent(vnode, container)
+    }
+}
+
+function processElement(vnode: any, container: any) {
+    mountElement(vnode, container)
+}
+
+function mountElement(vnode: any, container: any) {
+    const el = document.createElement(vnode.type)
+    const { children } = vnode
+
+    if (typeof children === 'string') {
+        el.textContent = children
+    } else if (Array.isArray(children)) {
+        mountChildren(vnode, el)
+    }
+
+    const { props } = vnode
+    for (const key in props) {
+        const val = props[key]
+        el.setAttribute(key, val)
+    }
+
+    container.append(el)
+}
+
+function mountChildren(vnode: any, container: any) {
+    const { children } = vnode
+    children.forEach((v: any) => patch(v, container))
 }
 
 function processComponent(vnode: any, container: any) {
