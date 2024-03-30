e55ae09c74a5101a640815a30b569f6edb416786
kid <2713786149@qq.com>
2024-03-31 06:33:00 +0800

    实现解析 element 标签

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast";
 
+enum TagType {
+    Start,
+    End
+}
+
 export function baseParse(content: string) {
     const context = createParseContext(content)
     return createRoot(parseChildren(context))
@@ -9,13 +14,39 @@ function parseChildren(context: { source: string }) {
     const nodes: any = []
 
     let node;
-    if (context.source.startsWith("{{")) {
+
+    const s = context.source
+    if (s.startsWith("{{")) {
         node = parseInterpolation(context);
+    } else if (s[0] === "<") {
+        if (/[a-z]/i.test(s[1])) {
+            node = parseElement(context)
+        }
     }
 
     nodes.push(node);
 
     return nodes;
+}
+
+function parseElement(context: { source: string }) {
+    const element = parseTag(context, TagType.Start)
+    parseTag(context, TagType.End)
+    return element
+}
+
+function parseTag(context: { source: string }, type: TagType) {
+    const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+    const tag = match[1]
+    advanceBy(context, match[0].length)
+    advanceBy(context, 1)
+
+    if (type === TagType.End) return
+
+    return {
+        type: NodeTypes.ELEMENT,
+        tag,
+    }
 }
 
 function parseInterpolation(context: { source: string }) {
